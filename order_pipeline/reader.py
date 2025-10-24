3e0626888a738d8ba104813f71f3f780b4858685
EmeritusAli <emeritusali@gmail.com>
2025-10-24 11:53:29 +0100

    craete read_json that reads json file and handles format and empty files

--- order_pipeline/reader.py
+++ order_pipeline/reader.py
@@ -1,6 +1,5 @@
 import json
 import os.path
-# pseudocode
 
 class Reader:
     def __init__(self):
@@ -10,17 +9,19 @@ class Reader:
         #existence
         if not os.path.isfile(path):
             raise ValueError("File not found")
-        
+        #read
         try:
             with open(path, "r") as file:
                 data = json.load(file)
         except json.JSONDecodeError:
             raise ValueError("Not valid JSON")
-        
+        #not empty
         if len(data) == 0:
                 raise ValueError("File is empty")
+        #format validation
         if not isinstance(data, list) or not all(isinstance(d,dict) for d in data):
              raise ValueError("JSON format must be list of dictionaries")
+        return data
     
 
 
